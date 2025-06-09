97226dbbcb644dcdc21d7488e96d8bda0a3dcaef
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-06-09 21:05:34 +0200

    Added lsp keymaps

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -32,3 +32,33 @@ vim.diagnostic.config({
     },
   },
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or "n"
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+    end
+
+    map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+    map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+    map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+    map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+    map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+    map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+    map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+    map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+    map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+
+    map("<leader>cd", vim.diagnostic.open_float, "[C]ode [D]iagnostics for line") -- show diagnostics for line
+    map("<leader>d", function()
+      local config = vim.diagnostic.config()
+      if config and config.virtual_lines and config.virtual_lines.current_line then
+        vim.diagnostic.config({ virtual_lines = false })
+      else
+        vim.diagnostic.config({ virtual_lines = { current_line = true } })
+      end
+    end, "Toggle virtual lines diagnostics for current line")
+  end,
+})
