d21e46e3d81226e8b03c47098328d759f36a1854
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-06-29 10:04:43 +0200

    Added nvim-lspconfig

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,111 @@
+return {
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "mason-org/mason.nvim", opts = {} },
+    "mason-org/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    { "j-hui/fidget.nvim", opts = {} },
+    "saghen/blink.cmp",
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc, mode)
+          mode = mode or "n"
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+        end
+
+        map("grn", vim.lsp.buf.rename, "[R]e[n]ame")
+        map("gra", vim.lsp.buf.code_action, "[G]oto Code [A]ction", { "n", "x" })
+        map("grr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+        map("gri", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+        map("grd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+        map("grD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+        map("gO", require("telescope.builtin").lsp_document_symbols, "Open Document Symbols")
+        map("gW", require("telescope.builtin").lsp_dynamic_workspace_symbols, "Open Workspace Symbols")
+        map("grt", require("telescope.builtin").lsp_type_definitions, "[G]oto [T]ype Definition")
+        map("<leader>cd", vim.diagnostic.open_float, "[C]ode [D]iagnostics for line")
+        map("<leader>d", function()
+          local config = vim.diagnostic.config()
+          if config.virtual_lines and config.virtual_lines.current_line then
+            vim.diagnostic.config({ virtual_lines = false })
+          else
+            vim.diagnostic.config({ virtual_lines = { current_line = true } })
+          end
+        end, "Toggle virtual lines diagnostics for current line")
+        ---@param client vim.lsp.Client
+        ---@param method vim.lsp.protocol.Method
+        ---@param bufnr? integer
+        ---@return boolean
+        local function client_supports_method(client, method, bufnr)
+          if vim.fn.has("nvim-0.11") == 1 then
+            return client:supports_method(method, bufnr)
+          else
+            return client.supports_method(method, { bufnr = bufnr })
+          end
+        end
+
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+          map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+          end, "[T]oggle Inlay [H]ints")
+        end
+      end,
+    })
+
+    -- Diagnostic Config
+    -- See :help vim.diagnostic.Opts
+    vim.diagnostic.config({
+      severity_sort = true,
+      float = { source = "if_many" },
+      underline = { severity = vim.diagnostic.severity.ERROR },
+      signs = vim.g.have_nerd_font and {
+        text = {
+          [vim.diagnostic.severity.ERROR] = "󰅚 ",
+          [vim.diagnostic.severity.WARN] = "󰀪 ",
+          [vim.diagnostic.severity.INFO] = "󰋽 ",
+          [vim.diagnostic.severity.HINT] = "󰌶 ",
+        },
+      } or {},
+      -- virtual_text = false,
+      -- virtual_text = {
+      --   source = 'if_many',
+      --   spacing = 2,
+      --   format = function(diagnostic)
+      --     local diagnostic_message = {
+      --       [vim.diagnostic.severity.ERROR] = diagnostic.message,
+      --       [vim.diagnostic.severity.WARN] = diagnostic.message,
+      --       [vim.diagnostic.severity.INFO] = diagnostic.message,
+      --       [vim.diagnostic.severity.HINT] = diagnostic.message,
+      --     }
+      --     return diagnostic_message[diagnostic.severity]
+      --   end,
+      -- },
+    })
+
+    local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+    local servers = {}
+
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, {
+      "stylua",
+    })
+    require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
+
+    require("mason-lspconfig").setup({
+      ensure_installed = {},
+      automatic_installation = false,
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+          require("lspconfig")[server_name].setup(server)
+        end,
+      },
+    })
+  end,
+}
