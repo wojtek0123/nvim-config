9a31adad4e72a5690fddc4aec74c1bb4d9ef85e0
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-07-01 20:25:29 +0200

    Update diagnostic config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -56,34 +56,28 @@ return {
       end,
     })
 
-    -- Diagnostic Config
-    -- See :help vim.diagnostic.Opts
     vim.diagnostic.config({
+      -- virtual_lines = true,
+      -- virtual_text = true,
+      underline = true,
+      update_in_insert = false,
       severity_sort = true,
-      float = { source = "if_many" },
-      underline = { severity = vim.diagnostic.severity.ERROR },
-      signs = vim.g.have_nerd_font and {
+      float = {
+        border = "rounded",
+        source = true,
+      },
+      signs = {
         text = {
           [vim.diagnostic.severity.ERROR] = "󰅚 ",
           [vim.diagnostic.severity.WARN] = "󰀪 ",
           [vim.diagnostic.severity.INFO] = "󰋽 ",
           [vim.diagnostic.severity.HINT] = "󰌶 ",
         },
-      } or {},
-      -- virtual_text = false,
-      -- virtual_text = {
-      --   source = 'if_many',
-      --   spacing = 2,
-      --   format = function(diagnostic)
-      --     local diagnostic_message = {
-      --       [vim.diagnostic.severity.ERROR] = diagnostic.message,
-      --       [vim.diagnostic.severity.WARN] = diagnostic.message,
-      --       [vim.diagnostic.severity.INFO] = diagnostic.message,
-      --       [vim.diagnostic.severity.HINT] = diagnostic.message,
-      --     }
-      --     return diagnostic_message[diagnostic.severity]
-      --   end,
-      -- },
+        numhl = {
+          [vim.diagnostic.severity.ERROR] = "ErrorMsg",
+          [vim.diagnostic.severity.WARN] = "WarningMsg",
+        },
+      },
     })
 
     local capabilities = require("blink.cmp").get_lsp_capabilities()
