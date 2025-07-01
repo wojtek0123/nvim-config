5743d27535210ce01aa1c98046c51db09e63c747
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2025-07-01 20:14:15 +0200

    Update yazi keymap

--- lua/plugins/yazi.lua
+++ lua/plugins/yazi.lua
@@ -8,21 +8,19 @@ return {
     "folke/snacks.nvim",
   },
   keys = {
-    -- 👇 in this section, choose your own keymappings!
     {
-      "<leader>cf",
+      "<leader>e",
       mode = { "n", "v" },
       "<cmd>Yazi<cr>",
       desc = "Open yazi at the current file",
     },
     {
-      -- Open in the current working directory
       "<leader>cw",
       "<cmd>Yazi cwd<cr>",
       desc = "Open the file manager in nvim's working directory",
     },
     {
-      "<c-up>",
+      "<leader>W",
       "<cmd>Yazi toggle<cr>",
       desc = "Resume the last yazi session",
     },
@@ -30,7 +28,7 @@ return {
   ---@type YaziConfig | {}
   opts = {
     -- if you want to open yazi instead of netrw, see below for more info
-    open_for_directories = false,
+    -- open_for_directories = true,
     keymaps = {
       show_help = "<f1>",
     },
