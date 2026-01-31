d64552917e353e3037803189049b232b5cba690b
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2026-01-31 09:52:30 +0100

    Update blink config

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,64 +1,20 @@
 return {
-  { "L3MON4D3/LuaSnip", keys = {} },
-  {
-    "saghen/blink.cmp",
-    dependencies = {
-      "rafamadriz/friendly-snippets",
+  "saghen/blink.cmp",
+  dependencies = { "rafamadriz/friendly-snippets" },
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    keymap = {
+      preset = "enter",
     },
-    version = "*",
-    config = function()
-      require("blink.cmp").setup({
-        snippets = { preset = "luasnip" },
-        signature = { enabled = true },
-        appearance = {
-          use_nvim_cmp_as_default = false,
-          nerd_font_variant = "normal",
-        },
-        sources = {
-          default = { "lsp", "snippets", "path", "buffer" },
-          providers = {
-            cmdline = {
-              min_keyword_length = 2,
-            },
-          },
-        },
-        keymap = {
-          preset = "enter",
-        },
-        cmdline = {
-          enabled = false,
-          completion = { menu = { auto_show = true } },
-          keymap = {
-            ["<CR>"] = { "accept_and_enter", "fallback" },
-          },
-        },
-        completion = {
-          menu = {
-            border = nil,
-            scrolloff = 1,
-            scrollbar = false,
-            draw = {
-              columns = {
-                { "kind_icon" },
-                { "label", "label_description", gap = 1 },
-                { "kind" },
-                { "source_name" },
-              },
-            },
-          },
-          documentation = {
-            window = {
-              border = nil,
-              scrollbar = false,
-              winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,EndOfBuffer:BlinkCmpDoc",
-            },
-            auto_show = true,
-            auto_show_delay_ms = 500,
-          },
-        },
-      })
-
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
+    appearance = {
+      nerd_font_variant = "normal",
+    },
+    completion = { documentation = { auto_show = true, auto_show_delay_ms = 500 } },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    fuzzy = { implementation = "prefer_rust" },
   },
+  opts_extend = { "sources.default" },
 }
