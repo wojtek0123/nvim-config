a72c6bd76b206595c7e087196eabc8b389b6cb64
Wojciech Pietraszuk <wojtekpietraszuk@gmail.com>
2026-03-21 14:04:08 +0100

    Restore inlay hints

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -82,13 +82,13 @@ return {
         --   end
         -- end
 
-        -- local client = vim.lsp.get_client_by_id(event.data.client_id)
-        --
-        -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
-        --   map("<leader>th", function()
-        --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
-        --   end, "[T]oggle Inlay [H]ints")
-        -- end
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+
+        if client and client:supports_method("textDocument/inlayHint", event.buf) then
+          map("<leader>th", function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
+          end, "[T]oggle Inlay [H]ints")
+        end
       end,
     })
   end,
